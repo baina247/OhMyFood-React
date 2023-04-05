412e8aabb0b10dacb258db60e84a4bd3575c0fa5
Hassani Assinaoui <hassani.assinaoui@gmail.com>
2023-04-05 17:49:39 +0200

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import StyledGlobalStyle from './utils/style/globalStyle'
+import Header from './components/header/index'
+import Home from './pages/home/index'
+import Propose from './pages/propose/index'
+import Become from './pages/become/index'
+import Notice from './pages/notice/index'
+import Contact from './pages/contact/index'
+import Error from './pages/error/index'
+import Footer from './components/footer/index'
+
+//Syntaxe React 18 import ReactDom via react-dom/client
+const root = document.getElementById('root')
+
+ReactDOM.createRoot(root).render(
+  //Syntaxe "Routes" à la place de "Switch" sur react-router-dom V6
+  <React.StrictMode>
+    <Router>
+      <StyledGlobalStyle />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/propose" element={<Propose />} />
+        <Route path="/become" element={<Become />} />
+        <Route path="/notice" element={<Notice />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </React.StrictMode>
+)
